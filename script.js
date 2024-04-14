4a8bda2106ca1d89371679108588378130c03517
priynshisaraf <priynshisaraf9@gmail.com>
2024-04-14 13:58:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,42 @@ function menuPop(){
         count =0;
     }
 }
+document.addEventListener("DOMContentLoaded", () => {
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+        // Accessing the user camera and video.
+        mediaDevices
+            .getUserMedia({
+                video: true,
+                audio: true,
+            })
+            .then((stream) => {
+                // Changing the source of video to current stream.
+                video.srcObject = stream;
+                video.addEventListener("loadedmetadata", () => {
+                    video.play();
+                });
+            })
+            .catch(alert);
+    });
+
+var photo=true;
+var video=false;
+function takeVideo(){
+    photo=false;
+    video=true;
+}
+function capture(){
+    if(photo==true){
+
+    }
+    else{
+        
+    }
+
+}
+function takePhoto(){
+    photo=true;
+    video=false;
+}
